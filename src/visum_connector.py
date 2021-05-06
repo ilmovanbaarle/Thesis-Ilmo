4d8f2d4b8f3063e8d10454574d4b502a322aa551
ilmovanbaarle <54939126+ilmovanbaarle@users.noreply.github.com>
2021-05-06 14:43:55 +0200

    Update visum_connector.py

--- src/visum_connector.py
+++ src/visum_connector.py
@@ -1,11 +1,42 @@
+wd = "C:/Users/nlilbm/Documents/Thesis/20201221 Mobilitievisie Groningen - complete/"
+
+def output_changer(df):
+    xls = pd.ExcelFile(df)
+    df1 = pd.read_excel(xls, 'SYNT_VERPL', index_col = 0, header = None, skiprows = 0,
+                    nrows = 1000, usecols = 'B:H')
+    
+    carshare_gemeente = df1.loc["TOTAAL"].iloc[1,0]/df1.loc["TOTAAL"].iloc[1,-1]
+    bikeshare_gemeente = df1.loc["TOTAAL"].iloc[1,3]/df1.loc["TOTAAL"].iloc[1,-1]
+    OVshare_gemeente = df1.loc["TOTAAL"].iloc[1,2]/df1.loc["TOTAAL"].iloc[1,-1]
+    totaal_gemeente = df1.loc["TOTAAL"].iloc[1,-1]
+    
+    output = {"carshare_gemeente":carshare_gemeente, "bikeshare_gemeente":bikeshare_gemeente,
+             "OVshare_gemeente":OVshare_gemeente,"totaal_gemeente":totaal_gemeente}
+
+    return output
 
 import os
-import win32com
+import win32com.client
 
-from ema_workbench.em_framework import FileModel, SingleReplication
-from ema_workbench.util import (method_logger, get_module_logger, debug,
-                                info)
-from ema_workbenc import ema_logging, perform_experiments
+from ema_workbench import ema_logging, perform_experiments
+
+import pandas as pd
+import decimal
+import math
+import numpy as np
+import os
+import warnings
+
+from ema_workbench.em_framework import TimeSeriesOutcome, FileModel
+from ema_workbench.em_framework.parameters import (Parameter, RealParameter,
+                                       CategoricalParameter)
+from ema_workbench.em_framework.util import NamedObjectMap
+from ema_workbench.em_framework.model import SingleReplication
+
+from ema_workbench.util import CaseError, EMAError, EMAWarning, get_module_logger
+from ema_workbench.util.ema_logging import method_logger
+
+from logging import DEBUG as debug
 
 _logger = get_module_logger(__name__)
 
@@ -15,7 +46,7 @@ class VisumModel(SingleReplication, FileModel):
     def __init__(self, name, wd=None, model_file=None, output_file=None,
                  n_replications=1):
         super(VisumModel, self).__init__(name, wd=wd, model_file=model_file)
-        self.visum = None
+        self.Visum = None
         self.n_replications = n_replications
         self.output_file = output_file
         self.path_to_output_file = None
@@ -27,12 +58,12 @@ class VisumModel(SingleReplication, FileModel):
         self.path_to_output_file = os.path.join(self.working_directory,
                                                 self.output_file)
 
-        debug("trying to start visum")
-        self.visum = win32com.client.Dispatch("Visum.Visum")
+        #debug("trying to start visum")
+        self.Visum = win32com.client.Dispatch("Visum.Visum")
 
-        debug(f"trying to load visum model from {path_to_model}")
-        self.visum.LoadVersion(path_to_model)
-        debug("model loaded succesfully")
+        #debug(f"trying to load visum model from {path_to_model}")
+        self.Visum.LoadVersion(path_to_model)
+        #debug("model loaded succesfully")
 
         # start visum
 
@@ -40,29 +71,34 @@ class VisumModel(SingleReplication, FileModel):
     def run_experiment(self, experiment):
 
         for k, v in experiment.items():
-            Visum.Net.SetAttValue(k, v)
+            self.Visum.Net.SetAttValue(k, v)
 
         # run experiment
         for i in range(self.n_replications):
             # in order to run te model
-            Visum.Procedures.Execute()
+            self.Visum.Procedures.Execute()
 
         # return outputs
-        output = pd.read_csv(self.path_to_output_file)
+        output = output_changer(self.path_to_output_file)
 
         results = {}
         for variable in self.output_variables:
 
             # assumption is that outcomes are stored by column
             # not sure if this is correct
-            results[variable] = output.loc[:, variable]
+            if variable in results:
+                tmp = results[variable]
+                tmp.append(output[variable])
+                results[variable] = tmp
+            if variable not in results:
+                results[variable] = [output[variable]]
 
         return results
 
 
 if __name__ == '__main__':
     from ema_workbench import (RealParameter, IntegerParameter, ScalarOutcome, Constant,
-                           Model, Policy)
+                           Model, Policy, MultiprocessingEvaluator)
     ema_logging.log_to_stderr(level=ema_logging.DEBUG)
 
     model = VisumModel('testmodel', wd=wd, model_file='Version for Thesis good.ver', output_file='TAB_GroVem_2040H_Iter1.csv',
@@ -73,56 +109,22 @@ if __name__ == '__main__':
                           RealParameter('KMKOSTENINDEX', 0.5 ,0.9),
                           RealParameter('OVKOSTENINDEX', 0.9, 1.1)]
 
-    model.outcomes = [ScalarOutcome('car_share'),
-                      ScalarOutcome('OV_share'),
-                      ScalarOutcome('total_km')]
+    model.outcomes = [ScalarOutcome('carshare_gemeente'),
+                      ScalarOutcome('bikeshare_gemeente'),
+                      ScalarOutcome('OVshare_gemeente'),
+                      ScalarOutcome('totaal_gemeente')]
 
     policies = [Policy('basecase',
-                      model_file = 'Version for Thesis good.ver'),
+                      model_file = 'Version for Thesis good.ver', output_file = 'Basecase.xlsx'),
                     Policy('fiets',
-                           model_file='Policy fiets.ver'),
+                           model_file='Policy fiets.ver', output_file = 'fiets.xlsx'),
                     Policy('30 km',
-                           model_file='Policy 30 km.ver'),
+                           model_file='Policy 30 km.ver', output_file = '30 km.xlsx'),
                     Policy('knips hoog',
-                           model_file='Policy knips hoog.ver')
+                           model_file='Policy knips hoog.ver', output_file = 'knips hoog.xlsx')
                     ]
 
 
     results = perform_experiments(scenarios=1, models = model, policies = policies)
-    
-    
-    ##########
-    
-[MainProcess/DEBUG] setting working directory to C:\Users\nlilbm\Documents\Thesis\20201221 Mobilitievisie Groningen - complete
-[MainProcess/INFO] performing 1 scenarios * 4 policies * 1 model(s) = 4 experiments
-[MainProcess/INFO] performing experiments sequentially
-[MainProcess/DEBUG] running scenario 16 for policy basecase on model testmodel
-[MainProcess/DEBUG] calling run_model on SingleReplication
-[MainProcess/DEBUG] calling run_model on AbstractModel
-[MainProcess/DEBUG] calling initialized on AbstractModel
-[MainProcess/DEBUG] completed calling initialized on AbstractModel
-[MainProcess/DEBUG] calling model_init on VisumModel
-[MainProcess/DEBUG] calling model_init on AbstractModel
-[MainProcess/DEBUG] completed calling model_init on AbstractModel
-[MainProcess/DEBUG] completed calling model_init on VisumModel
-[MainProcess/DEBUG] calling _transform on AbstractModel
-[MainProcess/DEBUG] completed calling _transform on AbstractModel
-[MainProcess/DEBUG] calling _transform on AbstractModel
-[MainProcess/DEBUG] completed calling _transform on AbstractModel
-[MainProcess/DEBUG] completed calling run_model on AbstractModel
-[MainProcess/DEBUG] calling run_experiment on VisumModel
-[MainProcess/ERROR] name 'Visum' is not defined
-Traceback (most recent call last):
-  File "C:\Users\nlilbm\Anaconda3\lib\site-packages\ema_workbench\em_framework\experiment_runner.py", line 85, in run_experiment
-    model.run_model(scenario, policy)
-  File "C:\Users\nlilbm\Anaconda3\lib\site-packages\ema_workbench\util\ema_logging.py", line 158, in wrapper
-    res = func(*args, **kwargs)
-  File "C:\Users\nlilbm\Anaconda3\lib\site-packages\ema_workbench\em_framework\model.py", line 338, in run_model
-    outputs = self.run_experiment(experiment)
-  File "C:\Users\nlilbm\Anaconda3\lib\site-packages\ema_workbench\util\ema_logging.py", line 158, in wrapper
-    res = func(*args, **kwargs)
-  File "<ipython-input-40-1a98a2fcc369>", line 56, in run_experiment
-    Visum.Net.SetAttValue(k, v)
-NameError: name 'Visum' is not defined
-[MainProcess/DEBUG] calling cleanup on AbstractModel
-[MainProcess/DEBUG] completed calling cleanup on AbstractModel
+    #with MultiprocessingEvaluator(model, n_processes=4) as evaluator:
+     #   results = evaluator.perform_experiments(4, policies=policies)
